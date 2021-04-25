9a8d442813006d05be667dba3abaa67dc55f1695
Alper KILIÇKAYA <alperkilickaya@gmail.com>
2021-04-26 00:00:32 +0300

    Armstrong Sayısı Bulma Ödevi

---
+++ armstrong.py
@@ -0,0 +1,14 @@
+a = input("Bir sayı giriniz: ").strip()
+if a.isdigit():
+  rakam = int(a)
+  liste = list(str(a))
+  basamak = len(liste)
+  toplam = 0
+  for i in liste:
+    toplam = toplam + (pow(int(i),basamak))
+  if toplam==rakam:
+    print(a, "sayısı Armstrong sayıdır.")
+  else:
+    print(a, "sayısı Armstrong sayı DEĞİLDİR.")
+else:
+ print ("lütfen uygun formatta giriniz!!")
